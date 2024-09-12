55c9e4bd6c5f7f2ba2ba369ec9fa6249f1b9f606
ENC_PoTaTo <ethan.carter@okbu.edu>
2024-09-12 14:43:28 -0500

    time-to-code

---
+++ battleship.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class battleship {
+
+   public static void main(String[] args) {
+      intro Intro = new intro();
+      boolean endSqu = false;
+      Char input1 = '1';
+      
+
+
+      while (!endSqu){
+         Intro.printInfo();
+
+
+
+      }
+   }
+}
---
+++ intro.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class intro {
+    private boolean Break;
+
+    public boolean endSqu(boolean break2){
+        return Break = true;        
+    }
+
+    public void printInfo(){
+    System.out.println("");
+    System.out.println("Welcome to Battle Ship");
+    System.out.println("");
+    System.out.println("please selet '1' to play or 'q' to Quit ");
+    }
+
+
+}
